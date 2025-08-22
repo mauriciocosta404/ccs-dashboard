6f336296e84a825c607dd4901495bee03c40e103
mcosta <mauricio.costa@xdsoftware.ao>
2025-08-22 15:48:01 +0100

    add blur propriety

--- src/components/landing-page/Navbar.tsx
+++ src/components/landing-page/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-white shadow-lg sticky w-full z-50 p-2">
+    <nav className="backdrop-blur-md bg-white/70 shadow-lg sticky top-0 w-full z-50 p-2">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
@@ -16,13 +16,13 @@ const Navbar = () => {
           </div>
           
           <div className="hidden md:flex items-center space-x-8">
-            <a href="#home" className="text-gray-700 hover:text-indigo-600">Início</a>
-            <a href="#about" className="text-gray-700 hover:text-indigo-600">Sobre</a>
-            <a href="#services" className="text-gray-700 hover:text-indigo-600">Cultos</a>
-            <a href="#events" className="text-gray-700 hover:text-indigo-600">Eventos</a>
-            <a href="#contact" className="text-gray-700 hover:text-indigo-600">Contato</a>
+            <a href="/#home" className="text-gray-700 hover:text-indigo-600">Início</a>
+            <a href="/#about" className="text-gray-700 hover:text-indigo-600">Sobre</a>
+            <a href="/#services" className="text-gray-700 hover:text-indigo-600">Cultos</a>
+            <a href="/#events" className="text-gray-700 hover:text-indigo-600">Eventos</a>
+            <a href="/#contact" className="text-gray-700 hover:text-indigo-600">Contato</a>
             <a href="/bible" className="text-gray-700 hover:text-indigo-600">Bíblia</a>
-            <a href="#contact" className="text-gray-700 hover:text-indigo-600">Hinário</a>
+            <a href="/#contact" className="text-gray-700 hover:text-indigo-600">Hinário</a>
             <a
               href="/signin"
               className="inline-block bg-indigo-600 text-white px-8 py-3 rounded-lg text-sm font-normal hover:bg-indigo-700 transition duration-300 animate-fadeIn animate-delay-200"
@@ -45,11 +45,11 @@ const Navbar = () => {
       {isOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a href="#home" className="block px-3 py-2 text-gray-700 hover:text-indigo-600">Início</a>
-            <a href="#about" className="block px-3 py-2 text-gray-700 hover:text-indigo-600">Sobre</a>
-            <a href="#services" className="block px-3 py-2 text-gray-700 hover:text-indigo-600">Cultos</a>
-            <a href="#events" className="block px-3 py-2 text-gray-700 hover:text-indigo-600">Eventos</a>
-            <a href="#contact" className="block px-3 py-2 text-gray-700 hover:text-indigo-600">Contato</a>
+            <a href="/#home" className="block px-3 py-2 text-gray-700 hover:text-indigo-600">Início</a>
+            <a href="/#about" className="block px-3 py-2 text-gray-700 hover:text-indigo-600">Sobre</a>
+            <a href="/#services" className="block px-3 py-2 text-gray-700 hover:text-indigo-600">Cultos</a>
+            <a href="/#events" className="block px-3 py-2 text-gray-700 hover:text-indigo-600">Eventos</a>
+            <a href="/#contact" className="block px-3 py-2 text-gray-700 hover:text-indigo-600">Contato</a>
             <a
               className="block px-3 py-2 text-gray-700 hover:text-indigo-600"
               href='/bible' 
