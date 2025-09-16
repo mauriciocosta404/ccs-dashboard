68a5c1361731043aa3f1f9fe5e7a0413bae194e0
mcosta <mauricio.costa@xdsoftware.ao>
2025-09-16 15:19:17 +0100

    remove eslint error

--- src/pages/sermons/register-sermon.tsx
+++ src/pages/sermons/register-sermon.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from "react";
 import ComponentCard from "../../components/common/ComponentCard";
 import PageBreadcrumb from "../../components/common/PageBreadCrumb";
