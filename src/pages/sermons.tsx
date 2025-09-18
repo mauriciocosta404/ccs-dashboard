d10edbb0d4a20adda4b06e8039c8c7835ed98edb
mcosta <mauricio.costa@xdsoftware.ao>
2025-09-18 14:20:28 +0100

    feat:improve style

--- src/pages/sermons.tsx
+++ src/pages/sermons.tsx
@@ -179,7 +179,7 @@ export function Sermons() {
             )}
           </div>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             {filteredSermons
               .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
               .map((sermon) => {
