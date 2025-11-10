113b84ffd58622a2275a11ba6818b62756d6d46b
mauriciocosta404 <mc611641@gmail.com>
2025-11-10 16:03:41 +0100

    style: adjust padding for WhatsApp button on landing page

--- src/components/landing-page/WhatsAppButton.tsx
+++ src/components/landing-page/WhatsAppButton.tsx
@@ -6,7 +6,7 @@ const WhatsAppButton = () => {
       href="https://wa.me/244931065964" // <-- substitui pelo teu número (código do país incluído)
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-20 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-all duration-300 z-50"
+      className="fixed bottom-24 right-6 bg-green-500 text-white md:p-4 p-6 rounded-full shadow-lg hover:bg-green-600 transition-all duration-300 z-50"
     >
       <FaWhatsapp />
     </a>
